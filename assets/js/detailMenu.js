ecba4e812441e7ad9789ca25c37272699c16ee80
rikimuhammadasli021299 <rikimuhammadasli@gmail.com>
2023-11-12 16:27:46 +0700

    add page detail menu

---
+++ assets/js/detailMenu.js
@@ -0,0 +1,50 @@
+let date;
+let month;
+let year;
+
+const today = new Date();
+
+month = today.getMonth();
+date = today.getDate();
+year = today.getFullYear();
+
+switch (month) {
+  case 0:
+    month = 'January';
+    break;
+  case 1:
+    month = 'February';
+    break;
+  case 2:
+    month = 'March';
+    break;
+  case 3:
+    month = 'April';
+    break;
+  case 4:
+    month = 'Mei';
+    break;
+  case 5:
+    month = 'June';
+    break;
+  case 6:
+    month = 'July';
+    break;
+  case 7:
+    month = 'August';
+    break;
+  case 8:
+    month = 'September';
+    break;
+  case 9:
+    month = 'October';
+    break;
+  case 10:
+    month = 'November';
+    break;
+  case 11:
+    month = 'December';
+    break;
+}
+
+document.getElementById('date').innerHTML = `${date} ${month} ${year}`;
